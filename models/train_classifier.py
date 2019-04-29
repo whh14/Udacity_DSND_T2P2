5190b9564bdadbf96612fee54ad20d20edb41b0f
whh14 <32293358+whh14@users.noreply.github.com>
2019-04-29 22:02:25 +1200

    upload the updated train_classifier.py

--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,150 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
+import pickle
+import re
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+
+nltk.download(['punkt', 'stopwords', 'wordnet'] )
 
 
 def load_data(database_filepath):
-    pass
+    """load data function.
+
+    load the sqlite database as pd.Dataframe.
+
+    Args:
+        database_filepath: str, input file path.
+
+    Returns:
+        X: pd.DataFrame, feature variable
+        Y: pd.DataFrame, target variable
+        category_names: list of category names of target variables
+
+    """
+
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('df', engine)
+
+    X = df['message']
+    Y = df.iloc[:, 4:]
+    category_names = Y.columns.tolist()
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    """tokenize function.
+
+    Apply tokenization to the text string for machine learning algorithm.
+
+    Args:
+        text: str, input string text to be tokenize.
+
+    Returns:
+        clean_tokens: list, list of token for machine learning algorithm
+
+    """
+
+    # Normalize text
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+
+    # Tokenize text
+    words = word_tokenize(text)
+
+    # Remove stop words
+    tokens = [w for w in words if w not in stopwords.words("english")]
+
+    # Lemmatization
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """build_model function.
+
+    build machine learning pipeline with GridsearchCV.
+
+    Args:
+        None.
+
+    Returns:
+        cv: GridSearchCV model
+
+    """
+
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+
+    parameters = {
+        'vect__ngram_range': ((1, 1), (1, 2)),
+        'vect__max_df': (0.5, 1.0),
+        'vect__max_features': (None, 10000),
+        'tfidf__use_idf': (True, False),
+        # 'clf__n_estimators': [50, 100, 200],
+        # 'clf__min_samples_split': [2, 3, 4],
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, verbose=2, cv=3, n_jobs=-1)
+
+    return cv
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """evaluate model function.
+
+    Evaluate model model performacne on the test set
+
+    Args:
+        model: model to be evaluated
+        X_test: feature variable of the testing set
+        Y_test: target variable of the testing set
+        category_names: category names of target variables
+
+    Returns:
+        None
+
+    """
+
+    y_pred = model.predict(X_test)
+    print(classification_report(Y_test, y_pred, target_names=category_names))
 
 
 def save_model(model, model_filepath):
-    pass
+    """save model function.
+
+    Save  model as a pickle file
+
+    Args:
+        model: model to be saved
+        model_filepath: str, file path of the pickle file
+
+    Returns:
+        None
+
+    """
+
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
 
 
 def main():
@@ -32,7 +158,7 @@ def main():
         model = build_model()
         
         print('Training model...')
-        model.fit(X_train, Y_train)
+        model.fit(X_train, Y_train.values)
         
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
